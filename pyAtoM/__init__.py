6590175bc2c0d8336caa5410c906ce38c67af46f
James Carr <carj@users.noreply.github.com>
2024-10-16 08:55:03 +0100

    Update __init__.py

--- pyAtoM/__init__.py
+++ pyAtoM/__init__.py
@@ -1,1 +1,15 @@
+"""
+pyAtoM package definition
 
+author:     James Carr
+licence:    Apache License 2.0
+
+"""
+
+
+__author__ = "James Carr (drjamescarr@gmail.com)"
+
+# Version of the pyPreservica package
+__version__ = "0.0.1"
+
+__license__ = "Apache License Version 2.0"
